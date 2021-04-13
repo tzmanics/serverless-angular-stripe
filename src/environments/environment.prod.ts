90b738c9facb801cc617d2df7445508e69abde09
Tara Z. Manicsic <tzmanics@gmail.com>
2021-04-13 04:27:41 -0400

    adds stripe key to envs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  stripe_key:
+    'pk_test_51Ifd6MKLDJNtfhU3KFgpHondS1v5gcOZrBu7m9vIJesnxoVhIf3KvPXeDAgwwNqHhnf9C3OXuuHVo9JnpJAqiXEq000ea7xfFe',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  stripe_key:
+    'pk_test_51Ifd6MKLDJNtfhU3KFgpHondS1v5gcOZrBu7m9vIJesnxoVhIf3KvPXeDAgwwNqHhnf9C3OXuuHVo9JnpJAqiXEq000ea7xfFe',
 };
 
 /*
